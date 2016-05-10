7d202b7ff6b532faa76296b53a0dbda7a0139196
Aimee <Aimee@Aimee-PC>
2016-05-10 15:23:08 -0300

    novoProduto

--- Projeto_BioID-war/build/web/_scripts/home/home.js
+++ Projeto_BioID-war/build/web/_scripts/home/home.js
@@ -1,7 +1,9 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+$(".circulo").mouseenter(function(){
+   $(".circulo").css("background", "radial-gradient(#ff3333, rgba(0, 36, 4, 0.8))");
+   $(".circulo .fa").css("color", "#fff");
+   $(".circulo .fa").css("text-shadow", "2px 2px #000");
+}).mouseleave(function(){
+   $(".circulo").css("background", "radial-gradient(#b8d2b8, rgba(0, 36, 4, 0.8))");
+   $(".circulo .fa").css("color", "#000");
+   $(".circulo .fa").css("text-shadow", "2px 2px #C3CFC4");
+});
