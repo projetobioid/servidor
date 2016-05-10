232d9f48e7e9ff5d257b92eb98c163cd989ac4ff
Aimee <Aimee@Aimee-PC>
2016-05-10 16:29:56 -0300

    novoProduto

--- Projeto_BioID-war/build/web/_scripts/home/home.js
+++ Projeto_BioID-war/build/web/_scripts/home/home.js
@@ -1,9 +1,49 @@
-$(".circulo").mouseenter(function(){
-   $(".circulo").css("background", "radial-gradient(#ff3333, rgba(0, 36, 4, 0.8))");
-   $(".circulo .fa").css("color", "#fff");
-   $(".circulo .fa").css("text-shadow", "2px 2px #000");
+function passouMouse(campo){
+   $(campo).css("background", "radial-gradient(#ff3333, rgba(0, 0, 0, 0.9))");
+   $(campo).css("cursor", "all-scroll");
+   $(campo +" .fa").css("color", "#fff");
+   $(campo +" .fa").css("text-shadow", "2px 2px #000");
+   $(campo+"F").css("font-size", "40px");
+   
+
+}
+
+function saiuMouse(campo){
+   $(campo).css("background", "radial-gradient(#b8d2b8, rgba(0, 0, 0, 0.9))");
+   //$(campo +" .fa").css("color", "#000");
+   $(campo +" .fa").css("text-shadow", "2px 2px #fff");
+   $(campo+"F").css("font-size", "30px");
+   
+      if(campo === "#agricultores"){
+        $(campo +" .fa").css("color", "#ff4040");
+        $(campo).css("background", "radial-gradient(#b8d2b8, rgba(221, 81, 116, 0.9))");
+   }else if(campo === "#produtos"){
+       $(campo +" .fa").css("color", "#06ff00");
+       $(campo).css("background", "radial-gradient(#b8d2b8, rgba(23, 119, 105, 0.9))");
+   }else{
+       $(campo +" .fa").css("color", "#6975f8");
+       $(campo).css("background", "radial-gradient(#b8d2b8, rgba(0, 0, 90, 0.9))");
+   }
+}
+
+
+//mouse em cima do produto
+$("#produtos").mouseenter(function(){
+    passouMouse("#produtos");
 }).mouseleave(function(){
-   $(".circulo").css("background", "radial-gradient(#b8d2b8, rgba(0, 36, 4, 0.8))");
-   $(".circulo .fa").css("color", "#000");
-   $(".circulo .fa").css("text-shadow", "2px 2px #C3CFC4");
+    saiuMouse("#produtos");
+});
+
+//mouse em cima do agricultores
+$("#agricultores").mouseenter(function(){
+    passouMouse("#agricultores");
+}).mouseleave(function(){
+    saiuMouse("#agricultores");
+});
+
+//mouse em cima do relatorios
+$("#relatorios").mouseenter(function(){
+    passouMouse("#relatorios");
+}).mouseleave(function(){
+    saiuMouse("#relatorios");
 });
