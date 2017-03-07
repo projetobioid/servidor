da03849a8b8b0e6484cf063c0e1b5ea9cc144344
daniel <daniel@daniel-fundetec>
2017-03-07 11:46:29 -0300

    retirada da sessao de cada servico rest

--- Projeto_BioID-war/web/js/ip.js
+++ Projeto_BioID-war/web/js/ip.js
@@ -1,6 +1,6 @@
 
 //var ipServidor = "http://187.19.101.252:8088/Projeto_BioID-war"; //sistema rodando fora
-//var ipServidor = "http://bioid.ddns.net:8088/Projeto_BioID-war";//acesso fora com dns
+var ipServidor = "http://bioid.ddns.net:8088/Projeto_BioID-war";//acesso fora com dns
 //var ipServidor = "http://10.1.2.52:8080/Projeto_BioID-war"; //sistema teste interno
-var ipServidor = "http://10.2.10.200:8080/Projeto_BioID-war"; //sistema em producao
+//var ipServidor = "http://10.2.10.200:8080/Projeto_BioID-war"; //sistema em producao
 //var ipServidor = "http://localhost:8080/Projeto_BioID-war"; //sistema em producao
